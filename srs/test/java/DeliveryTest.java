3f381bfda1aebab34fc3cc6c9ffeee5a6752bb47
Eugen Sabanovskiy <eugensabanovskiy123@gmail.com>
2025-02-18 22:20:47 +0300

    selenidehw fixed

--- srs/test/java/DeliveryTest.java
+++ srs/test/java/DeliveryTest.java
@@ -1,7 +1,4 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -12,19 +9,15 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryTest {
 
-    @BeforeEach
-    void setUp() {
-        open("http://localhost:9999");
-    }
-
     @Test
     void shouldSubmitRequest() {
-      
+        open("http://localhost:9999");
+
         String deliveryDate = LocalDate.now().plusDays(3)
                 .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A", Keys.BACK_SPACE); 
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A", Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(deliveryDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79270000000");
@@ -34,17 +27,5 @@ public class DeliveryTest {
         $("[data-test-id=notification]")
                 .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + deliveryDate))
                 .shouldBe(Condition.visible);
-    }
-    @Test
-    void shouldSelectCityFromDropdown() {
-        $("[data-test-id=city] input").setValue("Мо");
-        $$(".menu-item__control").findBy(Condition.text("Москва")).click();
-    }
-    @Test
-    void shouldSelectDateFromCalendar() {
-        $("[data-test-id=date] input").click();
-        LocalDate targetDate = LocalDate.now().plusDays(7);
-        String targetDay = String.valueOf(targetDate.getDayOfMonth());
-        $$(".calendar__day").findBy(Condition.text(targetDay)).click();
     }
 }
